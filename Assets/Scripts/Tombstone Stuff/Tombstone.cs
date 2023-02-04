518344c3f0bb450f849ffe5745c4f1d0fb08cc8d
Eddy20015 <edwardj5@hotmail.com>
2023-02-04 15:37:22 -0800

    Tombstone now has selection

--- Assets/Scripts/Tombstone Stuff/Tombstone.cs
+++ Assets/Scripts/Tombstone Stuff/Tombstone.cs
@@ -6,6 +6,8 @@ using Photon.Realtime;
 
 public abstract class Tombstone : MonoBehaviour
 {
+    public bool Selected;
+
     protected PhotonView view;
 
     protected void Start()
@@ -25,5 +27,27 @@ public abstract class Tombstone : MonoBehaviour
         {
             gameObject.SetActive(false);
         }
+    }
+
+    public void Select()
+    {
+        view.RPC("RPC_Select", RpcTarget.All);
+    }
+
+    public void Deselect()
+    {
+        view.RPC("RPC_Deselect", RpcTarget.All);
+    }
+
+    [PunRPC]
+    public void RPC_Select()
+    {
+        Selected = true;
+    }
+
+    [PunRPC]
+    public void RPC_Deselect()
+    {
+        Selected = false;
     }
 }
