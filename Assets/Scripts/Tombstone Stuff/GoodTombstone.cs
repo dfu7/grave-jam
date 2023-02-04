5e6241998693c0fb20f6fcc59a4087f3e913b7d4
dfu7 <78887201+dfu7@users.noreply.github.com>
2023-02-04 02:26:53 -0800

    tombstone thigns

---
+++ Assets/Scripts/Tombstone Stuff/GoodTombstone.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodTombstone : Tombstone
+{
+    public override void Effect()
+    {
+        Debug.Log("i love everything1! happy  :^)");
+    }
+}
---
+++ Assets/Scripts/Tombstone Stuff/TombstoneSpawner.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TombstoneSpawner : MonoBehaviour
+{
+    [SerializeField] private int numOfGoodTombstones = 19;
+    [SerializeField] private int numOfBadTombstones = 6;
+    [SerializeField] private Tombstone goodTombstone;
+    [SerializeField] private Tombstone badTombstone;
+    [SerializeField] private GameObject graveyard;
+
+    private float gWidthR;
+    private float gLengthR;
+    private float baseSpawnY;
+
+    private void Awake()
+    {   // might subtract 1 from these to give space for fence
+        gWidthR = graveyard.GetComponent<MeshRenderer>().bounds.size.x / 2 - 2;
+        gLengthR = graveyard.GetComponent<MeshRenderer>().bounds.size.z / 2 - 2;
+        baseSpawnY = graveyard.transform.position.y + graveyard.GetComponent<MeshRenderer>().bounds.size.y + 0.3f;
+
+        for (int i = 0; i < numOfGoodTombstones; i++)
+        {
+            Instantiate(goodTombstone, randomPos(), Quaternion.identity);
+        }
+
+        for (int i = 0; i < numOfBadTombstones; i++)
+        {
+            Instantiate(badTombstone, randomPos(), Quaternion.identity);
+        }
+    }
+
+    private Vector3 randomPos()
+    {
+        int x = (int)Random.Range(-gWidthR, gWidthR);
+        int z = (int)Random.Range(-gLengthR + 1, gLengthR);
+        return new Vector3(x, baseSpawnY, z);
+    }
+
+}
