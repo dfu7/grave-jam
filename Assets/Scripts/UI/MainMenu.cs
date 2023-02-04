4e565168c5a6921eed4b0327f1aba430b4b68a66
Eddy20015 <edwardj5@hotmail.com>
2023-02-03 23:58:09 -0800

    CrappyMainMenu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class MainMenu : MonoBehaviourPunCallbacks
+{
+    //this is the script that handles the buttons in the main menu
+
+    [SerializeField] private string ConnectToServer;
+
+    private bool OneFrame = false;
+
+    private void Update()
+    {
+        //calls all of the photon leave room an disconnections
+        if (!OneFrame)
+        {
+            PhotonNetwork.LeaveRoom();
+            OneFrame = true;
+        }
+    }
+
+    public override void OnLeftRoom()
+    {
+        PhotonNetwork.Disconnect();
+        //Debug.LogError("Disconnect Called");
+    }
+
+    //Enter the Lobby
+    public void OnClickOnlineMultiplayer()
+    {
+        PhotonNetwork.LoadLevel(ConnectToServer);
+    }
+
+    //Quit the Game
+    public void OnClickQuitGame()
+    {
+        Application.Quit();
+    }
+}
