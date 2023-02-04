9ee029c1dbf5d1a57925baba9f1ae2208d2b0d1d
Eddy20015 <edwardj5@hotmail.com>
2023-02-04 13:55:59 -0800

    ScoreManager and Tombstone Photonization
    ScoreManager shouuuuuld work

--- Assets/Scripts/Tombstone Stuff/TombstoneSpawner.cs
+++ Assets/Scripts/Tombstone Stuff/TombstoneSpawner.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class TombstoneSpawner : MonoBehaviour
+public class TombstoneSpawner : MonoBehaviourPunCallbacks
 {
     public int numOfGoodTombstones = 19;
     public int numOfBadTombstones = 6;
+    [SerializeField] private string goodTombstoneName;
+    [SerializeField] private string badTombstoneName;
     [SerializeField] private Tombstone goodTombstone;
     [SerializeField] private Tombstone badTombstone;
     [SerializeField] private GameObject graveyard;
@@ -16,21 +20,42 @@ public class TombstoneSpawner : MonoBehaviour
     private float gLengthR;
     private float baseSpawnY;
 
+    public bool UsingPhoton;
+
     private void Awake()
     {   // might subtract 1 from these to give space for fence
         gWidthR = graveyard.GetComponent<MeshRenderer>().bounds.size.x / 2 - 2;
         gLengthR = graveyard.GetComponent<MeshRenderer>().bounds.size.z / 2 - 2;
         baseSpawnY = graveyard.transform.position.y + graveyard.GetComponent<MeshRenderer>().bounds.size.y + 0.3f;
 
-        for (int i = 0; i < numOfGoodTombstones; i++)
+        if (!UsingPhoton)
         {
-            Instantiate(goodTombstone, randomPos(), Quaternion.identity);
-        }
+            for (int i = 0; i < numOfGoodTombstones; i++)
+            {
+                Instantiate(goodTombstone, randomPos(), Quaternion.identity);
+            }
 
-        for (int i = 0; i < numOfBadTombstones; i++)
-        {
-            Instantiate(badTombstone, randomPos(), Quaternion.identity);
+            for (int i = 0; i < numOfBadTombstones; i++)
+            {
+                Instantiate(badTombstone, randomPos(), Quaternion.identity);
+            }
         }
+        else
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                
+                for (int i = 0; i < numOfGoodTombstones; i++)
+                {
+                    PhotonNetwork.Instantiate(goodTombstoneName, randomPos(), Quaternion.identity);
+                }
+
+                for (int i = 0; i < numOfBadTombstones; i++)
+                {
+                    PhotonNetwork.Instantiate(badTombstoneName, randomPos(), Quaternion.identity);
+                }
+            }  
+        } 
     }
 
     private Vector3 randomPos()
